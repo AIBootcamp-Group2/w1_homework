aab85365dccb0fd7a4c4d281f3ef51091a950703
andres-pb <andres-pb10@hotmail.com>
2024-03-22 16:27:46 -0500

    main initial instructions

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-possible_inputs = {
-    1:"Suggesting dishes based on the following ingredients: \n",
-    2:"giving recipes to dishes",
-    3:"Criticize the following recipe: \n" 
-}
+from openai import OpenAI
 
+client = OpenAI()
+
+possible_inputs = {
+    '1':"Suggest me a dish based on the following ingredients ", 
+    '2':"Can you give me a recipe for the following dich? ",
+    '3':"Criticize the following recipe " 
+}
+print("\n The following are the possible premises: \n")
+for key, value in possible_inputs.items():
+    print(f"{key}. {value}")
+typed_key = input("\n Which one would yo like to use? \n (Type the number)")
+print(f"\n You selected: \n {possible_inputs[typed_key]}")
