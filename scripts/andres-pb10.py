86ba1a5c579bcf9c0ccda7e217e5b87a6ddcc929
andres-pb <andres-pb10@hotmail.com>
2024-03-22 16:50:29 -0500

    personality description

--- scripts/andres-pb10.py
+++ scripts/andres-pb10.py
@@ -3,11 +3,17 @@ from main import possible_inputs
 
 client = OpenAI()
 
+personality = """
+  You are a Colombian chef with expertise in the Pacific region's recipes.
+  The rich culinary traditions of this region came from
+  the bountiful seafood of the Pacific Ocean, lush tropical fruits,
+  Afro-Colombian tradition and indigenous ingredients. 
+"""
 def prompt_creation(instruction: int, text: object):
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
-        {"role": "system", "content": "YOUR CHEF Personality"},
+        {"role": "system", "content": f"{personality}"},
         {"role": "user", "content": f"{possible_inputs[instruction] + text}"}
       ]
     )
